225b2070a35aefbe0d06a69c340a5f781e5d2c07
ryan mckennon <ryan.mckennon@gmail.com>
2016-10-26 13:42:58 -0400

    Add the post deployment script

--- DotnetDatabase/MonsterDB/Script.PostDeployment.sql
+++ DotnetDatabase/MonsterDB/Script.PostDeployment.sql
@@ -10,3 +10,15 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------
 */
 
+use MonsterDB
+go
+
+
+insert into Monster.Gender(GenderName, Active) values (N'Female', 1), (N'Male', 1), (N'Alien', 1);
+go
+
+insert into Monster.MonsterType(TypeName, Active) values (N'Vampire',1), (N'Werewolf',1),(N'Zombie',1);
+go
+
+insert into Monster.Title(TitleName,Active) values (N'Sir',1),(N'Lady',1),(N'Count',1);
+go
