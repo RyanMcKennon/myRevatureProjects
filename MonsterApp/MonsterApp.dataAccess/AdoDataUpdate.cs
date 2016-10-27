49c8c1652f385e3da1cfc9f536f7bd5ab1a8bd10
ryan mckennon <ryan.mckennon@gmail.com>
2016-10-27 12:03:52 -0400

    Add update for gender

---
+++ MonsterApp/MonsterApp.dataAccess/AdoDataUpdate.cs
@@ -0,0 +1,34 @@
+﻿using MonsterApp.dataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterApp.dataAccess
+{
+    public partial class AdoData
+    {
+        public bool UpdateGender(Gender gender)
+        {
+
+            var query = "update Monster.Gender set Name = @name, Active = @active where GenderID = @id";
+            var name = new SqlParameter("name", gender.Name);
+            var active = new SqlParameter("active", gender.Active ? 1 : 0);
+            var id = new SqlParameter("id", gender.GenderID);
+            int result;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var cmd = new SqlCommand(query, connection);
+
+                cmd.Parameters.AddRange(new SqlParameter[] { id, name, active });
+                result = cmd.ExecuteNonQuery();
+            }
+
+            return result == 1;
+        }
+    }
+}
