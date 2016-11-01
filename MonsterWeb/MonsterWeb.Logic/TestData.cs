d110c8587defb043d0be45358b821f12e9026a44
ryan mckennon <ryan.mckennon@gmail.com>
2016-11-01 14:12:30 -0400

    Add web logic

---
+++ MonsterWeb/MonsterWeb.Logic/TestData.cs
@@ -0,0 +1,19 @@
+﻿using Monster.App.DataAcess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterWeb.Logic
+{
+    public class TestData
+    {
+        private MonsterService data = new MonsterService();
+
+        public void GetGender()
+        {
+            data.GetGender();
+        }
+    }
+}
